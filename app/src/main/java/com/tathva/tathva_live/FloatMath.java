fb8ac5998dfb88c9e7e13fc5928a9483a36a6b0a
gayathri <gayathrikooniyath@gmail.com>
2017-04-27 04:01:16 +0530

    second commit

---
+++ app/src/main/java/com/tathva/tathva_live/FloatMath.java
@@ -0,0 +1,19 @@
+package com.tathva.tathva_live;
+
+/**
+ * Created by gayathri on 26/4/17.
+ */
+
+public class FloatMath {
+    public static float cos(float t) {
+        return (float)Math.cos(t);
+    }
+
+    public static float sin(float t) {
+        return (float)Math.sin(t);
+    }
+
+    public static float sqrt(float t) {
+        return (float)Math.sqrt(t);
+    }
+}
